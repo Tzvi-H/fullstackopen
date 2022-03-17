118ab55d4de649b44f1d84b78c1a7a8a5b01e85b
Tzvi-H <16guitar@gmail.com>
2022-03-17 17:41:00 -0500

    completed 1.c

--- follow_along/part1/src/App.js
+++ follow_along/part1/src/App.js
@@ -1,7 +1,22 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>;
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0);
+
+  const increase = () => setCounter(counter + 1);
+  const decrease = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increase} text="increase"/>
+      <Button onClick={reset} text="reset"/>
+      <Button onClick={decrease} text="decrease"/>
+    </div>      
   )
 }
 
--- follow_along/part1/src/index.js
+++ follow_along/part1/src/index.js
@@ -1,17 +1,7 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-let counter = 1
-
-function refresh() {
-  ReactDOM.render(
-    <App counter={counter} />, 
-    document.getElementById('root')
-  )
-}
-
-refresh()
-setInterval(() => {
-  counter += 1;
-  refresh()
-}, 1000);
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
