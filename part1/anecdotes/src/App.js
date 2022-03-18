10d627be539aa44702acd4ce8e9b2a0e7b2ff190
Tzvi-H <16guitar@gmail.com>
2022-03-18 17:02:56 -0500

    completed 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [selected, setSelected] = useState(0)
-
-  const anecdotes = [
+const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -12,18 +10,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
-  const handleClick = () => {
+  const handleNextClick = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex)
   }
 
+  const handleVoteClick = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
-      <div>
-        <button onClick={handleClick}>next anecdote</button>
-      </div>
+      {anecdotes[selected]} <br/> has {votes[selected]} votes <br/>
+      <button onClick={handleVoteClick}>vote</button>
+      <button onClick={handleNextClick}>next anecdote</button>
     </div>
   )
 }
