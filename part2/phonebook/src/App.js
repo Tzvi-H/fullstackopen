44f0e039ab83c78f9f4ad0fe08d6924a22178787
Tzvi-H <16guitar@gmail.com>
2022-03-22 15:38:57 -0500

    completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newPerson, setNewPerson] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('');
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
 
   const handlePersonChange = event => {
     setNewPerson(event.target.value);
@@ -13,6 +21,10 @@ const App = () => {
 
   const handleNumberChange = event => {
     setNewNumber(event.target.value);
+  }
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
   }
 
   const addPerson = event => {
@@ -35,17 +47,27 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>
+          filter shown with <input onChange={handleFilterChange}/>
+        </p>
+      </div>
       <form onSubmit={addPerson}>
         <div>name: <input value={newPerson} onChange={handlePersonChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person => (
-          <p key={person.name}>{person.name} {person.number}</p>
+      <table>
+        <tbody>
+        {personsToShow.map(person => (
+          <tr key={person.name}>
+            <td>{person.name}</td> 
+            <td>{person.number}</td>
+          </tr>
         ))}
-      </div>
+        </tbody>
+      </table>
     </div>
   )
 }
