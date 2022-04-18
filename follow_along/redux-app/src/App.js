f787f954370177b560c999e763dc8e52babff6e0
Tzvi-H <16guitar@gmail.com>
2022-04-18 12:35:36 -0500

    completed 6.a

--- follow_along/redux-app/src/App.js
+++ follow_along/redux-app/src/App.js
@@ -1,28 +1,13 @@
-import { createStore } from 'redux'
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default: // if none of the above matches, code comes here
-      return state
-  }
+const App = () => {
+  return (
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
+  )
 }
 
-const store = createStore(counterReducer)
-store.subscribe(() => console.log(store.getState()))
-store.dispatch({type: 'INCREMENT'})
-store.dispatch({type: 'INCREMENT'})
-store.dispatch({type: 'INCREMENT'})
-store.dispatch({type: 'ZERO'})
-store.dispatch({type: 'DECREMENT'})
-
-const App = () => (
-  <p>{store.getState()}</p>
-)
-
-export default App;
+export default App
