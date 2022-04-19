4d7b977a667083af8552678aab4f8227794400b2
Tzvi-H <16guitar@gmail.com>
2022-04-19 12:10:35 -0500

    completed 6.b

--- follow_along/redux-app/src/index.js
+++ follow_along/redux-app/src/index.js
@@ -1,10 +1,19 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import App from './App'
+
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
