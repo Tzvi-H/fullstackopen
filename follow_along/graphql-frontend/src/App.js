19557005a638da7ebac5cb63058aa851c1d9e35e
Tzvi-H <16guitar@gmail.com>
2022-05-07 07:34:45 -0500

    completed 8.d

--- follow_along/graphql-frontend/src/App.js
+++ follow_along/graphql-frontend/src/App.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 import { ALL_PERSONS } from "./queries";
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
   const result = useQuery(ALL_PERSONS);
-
-  if (result.loading) {
-    return <div>loading...</div>;
-  }
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -21,21 +26,35 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
     </div>
   );
-};
-
-const Notify = ({ errorMessage }) => {
-  if (!errorMessage) {
-    return null;
-  }
-  return <div style={{ color: "red" }}>{errorMessage}</div>;
 };
 
 export default App;
